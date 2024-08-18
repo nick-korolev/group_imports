4e33523022f912b5adcbea13f68e3c4a6bafa969
nick-korolev <nullnoneundefined@gmail.com>
2024-08-19 00:38:06 +0200

    feat: change sort

--- src/formatter/fomatter.zig
+++ src/formatter/fomatter.zig
@@ -1,9 +1,23 @@
 const std = @import("std");
 const ImportDeclarationAst = @import("../ast/ast.zig").ImportDeclarationAst;
 
+fn getFirstPattern(source: []const u8) []const u8 {
+    if (std.mem.indexOf(u8, source, "/")) |slash_index| {
+        return source[0 .. slash_index + 1];
+    }
+    return source;
+}
+
 fn compare(context: void, a: ImportDeclarationAst, b: ImportDeclarationAst) bool {
     _ = context;
-    return a.source.value.len > b.source.value.len;
+    const pattern_a = getFirstPattern(a.source.value);
+    const pattern_b = getFirstPattern(b.source.value);
+
+    if (std.mem.eql(u8, pattern_a, pattern_b)) {
+        return std.mem.lessThan(u8, a.source.value, b.source.value);
+    }
+
+    return std.mem.lessThan(u8, pattern_a, pattern_b);
 }
 
 pub fn format(imports: *const std.ArrayList(ImportDeclarationAst)) !void {
